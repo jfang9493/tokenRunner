b97cae576f2f476b184fda8cabff7db94faa902f
Jackie Fang <jfang9493@bths.edu>
2018-11-15 09:57:42 -0500

    done

--- src/TokenPass.java
+++ src/TokenPass.java
@@ -1,19 +1,52 @@
 public class TokenPass {
     private int[] board;
-    private int playerCount;
+    private int currentPlayer;
 
     public TokenPass (int playerCount)
     {
         board = new int [playerCount];
         for (int i = 0; i <= board.length-1; i++)
         {
-            board[i] = (int) Math.random()*10+1;
+            board[i] = (int) ((Math.random()*10)+1);
         }
-        int currentPlayer = (int) Math.random()*(playerCount-1);
+        int currentPlayer = (int) (Math.random()*(playerCount-1));
     }
 
     public void distributeCurrentPlayerTokens()
     {
-        int token = board[currentPlayer];
+        int nextPlayer = currentPlayer;
+        int numToDistribute =  board[currentPlayer];
+        board[currentPlayer] = 0;
+        while (currentPlayer > 0 )
+        {
+            nextPlayer = (nextPlayer +1) % board.length;
+            board[nextPlayer]++;
+            numToDistribute--;
+        }
+    }
+
+    public void printBoard()
+    {
+        for (int i = 0; i <= board.length - 1;i++)
+        {
+            System.out.print("Player " + i + ": " + board[i] + " ");
+        }
+        System.out.print("\n");
+        System.out.print("Current Player: " + currentPlayer);
+        System.out.print("\n");
+    }
+
+    public void nextPlayer()
+    {
+        currentPlayer++;
+    }
+
+    public int gameOver()
+    {
+        for (int i = 0; i < board.length - 1; i++)
+        {
+            if (board[i] == 0) return i;
+        }
+        return -1;
     }
 }
